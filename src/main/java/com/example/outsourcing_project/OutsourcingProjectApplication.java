bb275b75389627e1252370609573b4fd21d60f9e
ioscoh <ssn100407@gmail.com>
2025-06-15 17:05:44 +0900

    Changes

---
+++ src/main/java/com/example/outsourcing_project/OutsourcingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.outsourcing_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutsourcingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutsourcingProjectApplication.class, args);
+    }
+
+}
